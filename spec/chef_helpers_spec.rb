0e58e3435dc7bc3eac362d0251a9992bcccab2e3
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-23 13:39:27 -0600

    Add Specs for #encrypt_password method.
    The #encrypt_password method should return a valid encrypted password
    hash and use SHA512 by default.

--- spec/chef_helpers_spec.rb
+++ spec/chef_helpers_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
-describe ChefHelpers do
+class Dummy; include ChefHelpers; end
+
+RSpec.describe ChefHelpers do
+  let(:dummy) { Dummy.new }
+  let(:password) { 'sekrit' }
+
   it 'has a version number' do
     expect(ChefHelpers::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#encrypt_password' do
+    it 'calls the method successfully' do
+      expect { dummy.encrypt_password(password) }.to_not raise_error
+    end
+
+    it 'returns an encrypted password' do
+      expect(dummy.encrypt_password(password)).to match(/^\$6\$/)
+    end
+
+    it 'returns a valid password hash' do
+      encrypted = dummy.encrypt_password(password)
+      expect(encrypted).to be_a_valid_password_hash_for(password)
+    end
   end
 end
