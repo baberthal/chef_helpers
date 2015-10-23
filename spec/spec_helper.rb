a990b3e2db183d373aa67cb6475de91ab023e8d5
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-23 13:10:48 -0600

    Add spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,19 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'chef_helpers'
+
+Dir['spec/support/**/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  Kernel.srand config.seed
+end
